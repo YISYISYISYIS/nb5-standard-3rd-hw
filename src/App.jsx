7d7b9ecb9a366ccf75fb528f857b56e45739d5bd
YIS <dtdt203@naver.com>
2024-05-20 20:00:25 +0900

    1번문제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ export default function App() {
     { name: "Charlie", age: 16, grade: "C" },
     { name: "Diana", age: 19, grade: "D" },
   ];
-
-  // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => student.age > 17);
+  const btnClick = (student) => {
+    return alert(`학생의 나이는 ${student.age}이고 점수는 ${student.grade}`);
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((student) => (
+          <li>
+            <button onClick={() => btnClick(student)}>{student.name}</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
